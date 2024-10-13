df80552c02aaec6b6fe1e46b4ec238ec364c3e35
Naimaanwarr <naimaanwarr@gmail.com>
2024-10-13 20:40:41 +0200

    MySongs

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,23 @@ import React from 'react';
 import {createRoot} from "react-dom";
 
 const root = createRoot(document.getElementById('root'));
-root.render(<h1>Hello react</h1>);
+
+function MySongs() {
+    return <form>
+        <label>
+        Song title: <input type={"text"}/>
+        </label>
+        <button type="submit">Add</button>
+    </form>;
+}
+
+function MusicApplication() {
+    return <>
+        <h1>My Favorite Songs </h1>
+
+        <MySongs />
+
+        </>;
+}
+
+root.render(<MusicApplication />);
